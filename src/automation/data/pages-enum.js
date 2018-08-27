a50f565075ae063f31e88eecca89251de46a4aa2
Sergey7198 <zinichuzh@gmail.com>
2018-08-27 10:31:01 +0300

    Add scenario main page

--- src/automation/data/pages-enum.js
+++ src/automation/data/pages-enum.js
@@ -1,10 +1,12 @@
 const loginPage = require('./page-selectors/login-page');
 const homePage = require('./page-selectors/home-page');
 const helpPage = require('./page-selectors/help-page');
+const mainPage = require('./page-selectors/main-page');
 
 module.exports = {
     loginPage,
     homePage,
-    helpPage
+    helpPage,
+    mainPage
     // should be added all application pages depends on project needs
 };

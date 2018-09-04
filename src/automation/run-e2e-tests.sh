c369d11635f6dc6529d364da47582fa002b89919
iana-kokriashkina1 <iana.kokriashkina@mercer.com>
2018-09-04 14:24:51 +0300

    update .sh file

--- src/automation/run-e2e-tests.sh
+++ src/automation/run-e2e-tests.sh
@@ -1,8 +1,6 @@
 cd src/automation
 npm install
-# Use on Jenkins to start selenium server and trash the verbose error messages from webdriver
-java -Dwebdriver.chrome.webdriver="/data/app/thirdparty-bin/chromedriver" -jar /opt/selenium/selenium-server-standalone.jar &
-# Wait for port 4444 to be listening connections
+npm run start-webdriver
 chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
 cd src/automation
 npm run test-local

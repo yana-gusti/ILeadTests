9397c2d7d4b010fc8e967d52958429781ed408b4
iana-kokriashkina1 <iana.kokriashkina@mercer.com>
2018-09-05 11:02:28 +0300

    update .sh file

--- src/automation/run-e2e-tests.sh
+++ src/automation/run-e2e-tests.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+#   Use this script to run tests
+cd src/automation
 npm install
-npm run start-webdriver
+npm run webdriver-update
+npm run webdriver &
+# Wait for port 4444 to be listening connections
 chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
-npm run test-local
+npm run test

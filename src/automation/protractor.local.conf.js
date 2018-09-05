11184866052c754de39002faa4ba4610e9898857
iana-kokriashkina1 <iana.kokriashkina@mercer.com>
2018-09-05 15:37:07 +0300

    update .sh file

--- src/automation/protractor.local.conf.js
+++ src/automation/protractor.local.conf.js
@@ -2,7 +2,7 @@ const {common_config, chromeCapabilities} = require('./common_config');
 
 exports.config = Object.assign({}, common_config, {
     capabilities: chromeCapabilities,
-    specs: ['features/ilead-features/func_test.feature']
-    //specs: ['features/ilead-features/reg_and_login.feature']
+    //specs: ['features/ilead-features/func_test.feature']
+    specs: ['features/ilead-features/reg_and_login.feature']
     //specs: ['features/ilead-features/*.feature']
 });

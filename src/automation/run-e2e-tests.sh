73fe2146e0955a05e5e7408c259174c5c91379af
iana-kokriashkina1 <iana.kokriashkina@mercer.com>
2018-09-06 11:16:57 +0300

    Update config

--- src/automation/run-e2e-tests.sh
+++ src/automation/run-e2e-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #   Use this script to run tests
 npm install
-npm prestart-webdriver
+npm run prestart-webdriver
 npm run start-webdriver &
 # Wait for port 4444 to be listening connections
 chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"

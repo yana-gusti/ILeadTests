60bdca64c6cfd7acb708ceb9344c76a781fdff14
yana-gusti <yana.gusti@gmail.com>
2018-09-06 12:11:36 +0300

    Update run-e2e-tests.sh

--- src/automation/run-e2e-tests.sh
+++ src/automation/run-e2e-tests.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #   Use this script to run tests
-npm install
-npm run prestart-webdriver
+pkill -f chrome
+npm i
 npm run start-webdriver &
 # Wait for port 4444 to be listening connections
-chmod +x wait-for-it.sh ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
+chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
 npm run test-local

5a3dafde85c29edaa47fd7710657ed72a8b6c72a
Iana Kokriashkina <yana@maxiru.com>
2018-09-06 12:35:11 +0300

    Change project structure

--- run-it.sh
+++ run-it.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 #   Use this script to run tests
-lsof -t -i :4444 | xargs kill
 npm i
 npm run webdriver-update
 npm run webdriver &
 # Wait for port 4444 to be listening connections
 chmod +x wait-for-it.sh && ./wait-for-it.sh -t 60 127.0.0.1:4444 -- echo "driver is up"
 npm run test
-lsof -t -i :4444 | xargs kill

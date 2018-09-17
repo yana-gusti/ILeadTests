664b402714524ab72b46c1ab0af23ef2ea800400
iana-kokriashkina1 <iana.kokriashkina@mercer.com>
2018-09-17 14:23:19 +0300

    update scenario with negative domain test

--- src/data/page-selectors/login-page.js
+++ src/data/page-selectors/login-page.js
@@ -1,14 +1,8 @@
-/**
- * This file is represent Page Objects usage.
- * Here should be CSS selectors related to application.
- * Any desired/appropriate nesting and structure can be used.
- */
-
 module.exports = {
     loginInput: '#username',
     passwordInput: '#password',
     loginButton: '#loginButton',
-    goToOlderFormButton: ".auth-older_link",
+    goToOlderFormButton: '.auth-older_link',
     emailfield: '#signInEmail',
     passwordfield: '#signInPassword',
     singInButton: '#logInBtn',
@@ -19,5 +13,5 @@ module.exports = {
     lastName:'#lastName',
     email:'#email',
     password:'#password',
-    confirmPassword: '#confirmPassword'
+    confirmPassword: '#confirmPassword',
 };
